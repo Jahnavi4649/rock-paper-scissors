dbb0a4896b680a1b18be15b210bfcc2159cb54d5
Yasuo67 <aomiii876@gmail.com>
2024-09-23 12:43:46 +0530

    updated playGame function to display score in an alert box after each round. Added a start button and basic css

--- game.js
+++ game.js
@@ -3,20 +3,19 @@ let humanScore = 0;
 let computerScore = 0;
 let message ="";
 
-// getComputerChoice() uses Math.random to randomly return rock/paper/scissors 
-function getComputerChoice(){
-    const min =0, max =2;
-    let choice =Math.floor(Math.random() * (max - min + 1) ) + min; //returns an integer between 0 and 2(both included)
-    return gameOptions[choice];
-}
-
+//funcion to get user input through prompt
 function getHumanChoice(){
-    let input = prompt("Enter your choice");
+    let input = prompt("Enter your choice: rock / paper / scissors");
     return input.toLowerCase() ;
 }
 
-playGame();
+// function to get computer input through Math.random()
+function getComputerChoice(){
+    let choice =Math.floor(Math.random() * gameOptions.length ) ; 
+    return gameOptions[choice];
+}
 
+//function to decide winner in each round and incrementing scores
 function playRound(humanChoice, computerChoice) {
     if(humanChoice == computerChoice){
         message = "Both choose same move!";
@@ -54,34 +53,41 @@ function playRound(humanChoice, computerChoice) {
     return message;
 }
 
- 
+//function to decide final winner and calls all other functions for 5 times(since the game contains 5 rounds) 
 function playGame(){
+
+    //loop for calling all functions for 5 times
     for(let i=0; i<5; i++){
-        const computerSelection = getComputerChoice();
         const humanSelection = getHumanChoice();
-        console.log("comp: "+computerSelection);
-        console.log("human: "+humanSelection);
+        if(humanSelection == "rock" || humanSelection == "paper" || humanSelection == "scissors"){
+            const computerSelection = getComputerChoice();
+            console.log("comp: "+computerSelection);
+            console.log("human: "+humanSelection);
 
-        let roundMessage = playRound(humanSelection, computerSelection);
-        // document.getElementById("message").innerHTML = roundMessage;
-        // console.log("message: " + message);
-        console.log("humanScore: " + humanScore + ", computerScore: " + computerScore);
+            let roundMessage = playRound(humanSelection, computerSelection);
+            console.log(roundMessage);
+            console.log("computer score: "+ computerScore +", human score: "+humanScore);
 
+            alert("Round: "+ (i+1) + "\nYour Choice: "+humanSelection +", Computer Choice: "+ computerSelection +"\nResult: "+roundMessage +"\nHuman Score: "+humanScore +", Computer Score: "+computerScore+" ");
+        }
+
+        else{
+            alert("Looks like the value you entered is incorrect. Please enter the right values");
+            i--;
+        }
         
     }
+
+    //declaring the highest scorer after 5 rounds as winner
     if(humanScore > computerScore){
-        console.log("winner: human");
+        document.getElementById("roundResults").innerText = ` Congrats! You Win!!`;
+    }
+    else if(humanScore == computerScore){
+        document.getElementById("roundResults").innerText = ` Uh-oh! It's a tie.`;
     }
     else{
-        console.log("winner: computer");
+        document.getElementById("roundResults").innerText = `Oops! You Lose!!`;
     }
-    
+    document.getElementById("btn").innerText = `Play Again`;
 }
 
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
-// console.log("comp: "+computerSelection);
-// console.log("human: "+humanSelection);
-  
-// playRound(humanSelection, computerSelection);
-  
